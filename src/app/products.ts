06ed08710f0a5a3378e5ea658beb9c25875c309e
narraboss <basanth.narra@gmail.com>
2019-09-19 18:43:35 -0400

    commit

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    image: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    image: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
